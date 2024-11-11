1b4606ae376a2ce5088b58c9212b6665b6d9e074
sdbarlow <sethdbarlow@gmail.com>
2024-11-11 09:32:52 -0600

    remove secrets

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()  # load environment variables from .env file
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'postgresql://username:password@localhost:5432/yourdatabase'
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
